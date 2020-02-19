707ce864546ea38e08c6dd372b6378352c59be31
Juniorlpes <bento.lopes50@gmail.com>
2020-02-19 00:13:36 -0300

    primeira versão consultas

---
+++ db/consultas.php
@@ -0,0 +1,30 @@
+<?php  
+	$servidor = "localhost";
+	$usuario = "root";
+	$senha = "root";
+	$dbname = "antares";
+	
+	//Conecta
+	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname) or die("Erro: ".mysql_error());
+
+	function getAlunos(){
+
+	}
+
+	function getEstagiarios(){
+		
+	}
+
+	function getEscolas(){
+		
+	}
+
+	function getTurmas(){
+		
+	}
+
+	function getEventos(){
+		
+	}
+
+?>
