d388116b08df2ef517b4bfa3ef0ea2c614a6285c
Juniorlpes <bento.lopes50@gmail.com>
2020-02-19 00:14:21 -0300

    primeira versão consultas

--- db/consultas.php
+++ db/consultas.php
@@ -1,29 +1,55 @@
 <?php  
-	$servidor = "localhost";
-	$usuario = "root";
-	$senha = "root";
-	$dbname = "antares";
-	
-	//Conecta
-	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname) or die("Erro: ".mysql_error());
+	include("conexao.php");
 
-	function getAlunos(){
+	function getAlunos($id_turma) : array{
+		$sql 	= "select nome from alunos where turma = ". $id_turma;
+		$result = mysqli_query($sql);
+		$row	= mysql_fetch_assoc($result);
 
+		return $row;
 	}
 
 	function getEstagiarios(){
-		
+		$sql 	= "select nome from usuarios where permissao = 1";
+		$result = mysqli_query($sql);
+		$row	= mysql_fetch_assoc($result);
+
+		return $row;
 	}
 
 	function getEscolas(){
-		
+		$sql 	= "select nome, id from usuarios where permissao = 3";
+		$result = mysqli_query($sql);
+		$row	= mysql_fetch_assoc($result);
+
+		return $row;
 	}
 
-	function getTurmas(){
-		
+	function getTurmas(): array{
+		$sql 	= "select nome_turma, escola from turmas";
+		$result = mysqli_query($sql);
+		$row	= mysql_fetch_assoc($result);
+
+		return $row;
 	}
 
-	function getEventos(){
+	function getTurmasFromEscola($id_escola): array{
+		$sql 	= "select nome_turma from turmas where escola = ".$id_escola;
+		$result = mysqli_query($sql);
+		$row	= mysql_fetch_assoc($result);
+
+		return $row;
+	}
+
+	function getEventos(): array{
+		$sql 	= "select title, descricao, vagas from eventos";
+		$result = mysqli_query($sql);
+		$row	= mysql_fetch_assoc($result);
+
+		return $row;
+	}
+
+	function getProfessores(){
 		
 	}
 
